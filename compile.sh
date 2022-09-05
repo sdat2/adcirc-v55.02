b3c428f7d1fa086c4bdc1043b5d24296b2c3cf86
Simon Thomas <sdat2@cam.ac.uk>
2022-09-05 17:18:51 +0100

    ext: still not workign for mpi in gfortran etc

--- compile.sh
+++ compile.sh
@@ -12,11 +12,10 @@ module load parallel-netcdf/gnu/20141122
 nf-config --all
 nc-config --all
 
-
 cd adcirc/work
 
 # turned on swan, seemed to work?
-cmake .. -DCMAKE_C_COMPILER="/usr/bin/gcc" -DCMAKE_CXX_COMPILER="/usr/bin/gcc" -DCMAKE_Fortran_COMPILER="/usr/bin/gfortran" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=OFF -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=ON -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -lnetcdff -DREAL8 -DCSCA -DLINUX -w -O2" -DNETCDF_LIBRARIES="/usr/lib64" -DNETCDF_LIBRARY="/usr/lib64/libnetcdf.so"
+cmake .. -DCMAKE_C_COMPILER="/usr/bin/gcc" -DCMAKE_CXX_COMPILER="/usr/bin/gcc" -DCMAKE_Fortran_COMPILER="/usr/bin/gfortran" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=OFF  -DBUILD_PADCSWAN=OFF -DENABLE_OUTPUT_NETCDF=OFF -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -lnetcdff -DREAL8 -DCSCA -DLINUX -w -O2" # -DNETCDF_LIBRARIES="/usr/lib64" -DNETCDF_LIBRARY="/usr/lib64/libnetcdf.so"
 
 # /usr/lib64/libnetcdf.so
 
