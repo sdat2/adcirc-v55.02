3730d43a6377c9fe16dac00a9c0c748bfd114990
Simon Thomas <sdat2@cam.ac.uk>
2022-09-06 09:25:58 +0100

    ext: ok

--- compile.sh
+++ compile.sh
@@ -10,20 +10,19 @@ module load netcdf/intel/14.0/4.3.2
 module load netcdff/intel/14.0/4.2
 module load parallel-netcdf/intel/20141122
 
-echo "** Moving into work directory"
+echo "** Moving into work directory **"
 
-cd adcirc
-cd work
+cd adcirc/work
 
-echo "** About to create makefile with cmake"
+echo "** About to create makefile with cmake **"
 
 # turned on swan, seemed to work?
-cmake .. -DCMAKE_C_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_CXX_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_Fortran_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/ifort" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=ON -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=OFF -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" 
+# cmake .. -DCMAKE_C_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_CXX_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_Fortran_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/ifort" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=ON -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=OFF -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" 
 
 # -DNETCDF_LIBRARIES="/opt/cray/pe/netcdf/4.7.4.3/bin" -DNETCDF_INCLUDE_DIRS="/opt/cray/pe/netcdf/4.7.4.3/bin" -DNETCDF_LIBRARY="/opt/cray/pe/netcdf/4.7.4.3/bin"
 
-echo "** Now moving onto compilation"
+echo "** Now moving onto compilation **"
 
 make
 
-echo "** Compilation attempted"
+echo "** Compilation attempted **"
