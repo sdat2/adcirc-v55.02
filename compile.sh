4802302d21d9eaf61e123985a091078a144affec
Simon D.A. Thomas <sdat2@cam.ac.uk>
2022-09-07 13:29:55 +0100

    ext: trying to fix mpi compiling

--- compile.sh
+++ compile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd adcirc
-cd work
+cd adcirc/work
 
 # turned on swan, seemed to work?
-/Users/simon/homebrew/bin/cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=OFF -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=OFF -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" -DNETCDF_LIBRARIES="/usr/local/Cellar/netcdf/4.7.4_2/bin" -DNETCDF_INCLUDE_DIRS="/usr/local/Cellar/netcdf/4.7.4_2/bin" -DNETCDF_LIBRARY="/usr/local/Cellar/netcdf/4.7.4_2/bin"
+cmake .. -DCMAKE_C_COMPILER="/opt/anaconda3/bin/mpicc" -DCMAKE_CXX_COMPILER="/opt/anaconda3/bin/mpicxx" -DCMAKE_Fortran_COMPILER="/opt/anaconda3/bin/mpifort" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=ON -DENABLE_OUTPUT_NETCDF=OFF -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=OFF -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" # -DNETCDF_LIBRARIES="/opt/homebrew/Cellar/netcdf/4.8.1_3/bin"
 
 make
