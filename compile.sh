2ae12eb85d9f6ea0038a1203cf02668c62094aa3
Simon Thomas <sdat2@cam.ac.uk>
2023-02-05 21:00:41 +0000

    small change

--- compile.sh
+++ compile.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-module load intel/20.0.0
-module load intel/cce/20.0.0
-module load eb/OpenMPI/intel/4.1.0
-module load intel/mpi/5.1.2.150
-module load libpnetcdf/intel/14.0/1.5.0
-module load libPHDF5/intel/14.0/1.8.12
-module load netcdf/intel/14.0/4.3.2
+module load intel/12.1.5
+module load intel/cce/12.1.0.233
+module load intel/mpi/4.1.2.040 
+module load intel/fce/12.1.0.233
+module load intel/idb/12.0
 module load netcdff/intel/14.0/4.2
-module load parallel-netcdf/intel/20141122
+module load netcdf/intel/14.0/4.3.2
+#module load intel/cce/20.0.0
+# module load eb/OpenMPI/intel/4.1.0
+#module load intel/mpi/5.1.2.150
+#module load libpnetcdf/intel/14.0/1.5.0
+#module load libPHDF5/intel/14.0/1.8.12
+#module load netcdf/intel/14.0/4.3.2
+#module load netcdff/intel/14.0/4.2
+#module load parallel-netcdf/intel/20141122
 
 echo "** Moving into work directory **"
 
@@ -17,7 +23,7 @@ cd adcirc/work
 echo "** About to create makefile with cmake **"
 
 # turned on swan, seemed to work?
-# cmake .. -DCMAKE_C_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_CXX_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_Fortran_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/ifort" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=ON -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132" -DBUILD_PADCSWAN=OFF -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" 
+cmake .. -DCMAKE_C_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_CXX_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/icc" -DCMAKE_Fortran_COMPILER="/apps/intel/2013_sp1.2.144/composer_xe_2013_sp1.2.144/bin/intel64/ifort" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=OFF -DBUILD_PADCSWAN=OFF -DBUILD_ADCPREP=OFF -DBUILD_ADCSWAN=OFF -DENABLE_OUTPUT_NETCDF=ON -DFortran_COMPILER_SPECIFIC_FLAG="-ffixed-line-length-132"  -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" 
 
 # -DNETCDF_LIBRARIES="/opt/cray/pe/netcdf/4.7.4.3/bin" -DNETCDF_INCLUDE_DIRS="/opt/cray/pe/netcdf/4.7.4.3/bin" -DNETCDF_LIBRARY="/opt/cray/pe/netcdf/4.7.4.3/bin"
 
