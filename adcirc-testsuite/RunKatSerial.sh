b0239a36feece7f053fc5c986a908ae956a5dc7d
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-04-09 11:52:36 +0100

    ok

---
+++ adcirc-testsuite/RunKatSerial.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#...List of ADCIRC's test cases. Cases are named by their directory tree
+case_list=( adcirc/adcirc_katrina-2d                                 \
+            adcirc/adcirc_katrina-2d-nws13                           )
+# @jasonfleming 20181206 : FIXME : adcirc+swan tests failing for unknown reasons
+#            adcirc/adcirc_shinnecock_inlet-parallel                  \
+#            adcirc-swan/adcirc_swan_apes_irene                       \
+#            adcirc-swan/adcirc_swan_apes_irene-parallel              )
+
+#...Maximum Error
+err=0.00001
+
+#...Path to the executables
+adcirc_path=$1
+
+#...Current home location
+TESTHOME=$(pwd)
+
+cont=0
+if [ $# -eq 2 ] ; then
+    if [ $2 == "--continue" ] ; then
+        cont=1
+    fi
+fi
+#
+#...Sanity check on script arguments
+if [ $# -ne 1 ] && [ $# -ne 2 ]  ; then
+    echo "ERROR: Script requires 1 argument with folder containing executables."
+    exit 1
+fi
+
+#...Ensure that a relative path is not supplied
+if [ "x${adcirc_path:0:1}" != "x/" ] ; then
+    echo "ERROR: You must provide an absoloute path."
+    exit 1
+fi
+
+#...Check if adcirc exists
+if [ ! -s $1/adcirc ] ; then
+    echo "ERROR: adcirc executable not found."
+    exit 1
+fi
+
+#...Check if adcprep exists
+if [ ! -s $1/adcprep ] ; then
+    echo "ERROR: adcprep executable not found."
+    exit 1
+fi
+
+#...Check if padcirc exists
+if [ ! -s $1/padcirc ] ; then
+    echo "ERROR: padcirc executable not found."
+    exit 1
+fi
+
+#...Check of adccmp exists
+if [ ! -s $1/adcircResultsComparison ] ; then
+    echo "ERROR: adcircResultsComparison executable not found."
+    exit 1
+fi
+
+#...Loop to run over all the test cases
+for CASE in ${case_list[@]}
+do
+
+    #...Proceed into the case directory
+    cd $CASE
+
+    #...Check for the generic run.sh script
+    if [ ! -s run.sh ]; then
+        echo "ERROR: Could not find run script for $CASE"
+        exit 1
+    fi
+    
+    #...Run the case and check the return status
+    ./run.sh $adcirc_path $err 2>/dev/null
+    if [ $? -ne 0 ] ; then
+        echo "ERROR: The case $CASE did not pass."
+        if [ $cont == 0 ] ; then
+            exit 1
+        fi
+    fi
+
+    #...Back to testing home location
+    cd $TESTHOME
+done
+
+#...Exit with status zero if all tests have passed
+exit 0
--- compile.sh
+++ compile.sh
@@ -7,7 +7,10 @@ echo $PATH
 cd adcirc/work
 
 # turned on swan, seemed to work?
-# cmake .. -DCMAKE_C_COMPILER="/opt/homebrew/Cellar/gcc" -DCMAKE_CXX_COMPILER="/opt/homebrew/Cellar/gcc" -DCMAKE_Fortran_COMPILER="/opt/homebrew/Cellar/gcc" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_SWAN=OFF -DBUILD_UTILITIES=OFF -DBUILD_ADCSWAN=OFF -DBUILD_PADCSWAN=OFF  -DBUILD_PUNSWAN=OFF -DADDITIONAL_FLAGS_SWAN="-ffixed-line-length-132 -ffree-line-length-132" -DENABLE_OUTPUT_NETCDF=ON -DNETCDF_LIBRARIES="/opt/homebrew/Cellar/netcdf/4.8.1_3/bin" -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132 -ffree-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -fallow-argument-mismatch -O2"
+cmake .. -DCMAKE_C_COMPILER="/opt/homebrew/Cellar/gcc" -DCMAKE_CXX_COMPILER="/opt/homebrew/Cellar/gcc" -DCMAKE_Fortran_COMPILER="/opt/homebrew/Cellar/gcc" -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_SWAN=ON -DBUILD_UTILITIES=ON -DBUILD_ADCSWAN=ON -DBUILD_PADCSWAN=ON  -DBUILD_PUNSWAN=ON -DADDITIONAL_FLAGS_SWAN="-ffixed-line-length-73 -ffree-line-length-73" -DENABLE_OUTPUT_NETCDF=ON -DNETCDF_LIBRARIES="/opt/homebrew/Cellar/netcdf/4.9.0/bin" -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132 -ffree-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -fallow-argument-mismatch -O2"
 
 # need to allow argument mismatch in modern fortran https://github.com/Unidata/netcdf-fortran/issues/212
-make
+
+cmake --build ..
+# make
+# cmakee 
