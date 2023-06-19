eaeb6ff21975ddd0f19b24c9f9fa8e3d80ecf9e6
Simon D.A. Thomas <sdat2@cam.ac.uk>
2023-06-19 13:03:41 +0100

    trying to compile

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ cd adcirc/work
 
 # turned on swan, seemed to work?
 gcc=/opt/homebrew/bin/gcc-12
-cmake .. -DCMAKE_C_COMPILER=$gcc -DCMAKE_CXX_COMPILER=$gcc -DCMAKE_Fortran_COMPILER=$gcc -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON -DBUILD_ADCPREP=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_LIBADCIRC_SHARED=ON -DBUILD_SWAN=ON -DBUILD_UTILITIES=ON -DBUILD_ADCSWAN=ON -DBUILD_PADCSWAN=ON  -DBUILD_PUNSWAN=ON -DADDITIONAL_FLAGS_SWAN="-ffixed-line-length-73 -ffree-line-length-73" -DENABLE_OUTPUT_NETCDF=ON -DNETCDF_LIBRARIES="/opt/homebrew/Cellar/netcdf/4.9.0/bin" -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132 -ffree-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -O2" #-fallow-argument-mismatch -O2"
+cmake .. -DCMAKE_C_COMPILER=$gcc -DCMAKE_CXX_COMPILER=$gcc -DCMAKE_Fortran_COMPILER=$gcc -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=OFF -DBUILD_ADCPREP=OFF -DBUILD_LIBADCIRC_SHARED=OFF -DBUILD_LIBADCIRC_SHARED=OFF -DBUILD_SWAN=OFF -DBUILD_UTILITIES=OFF -DBUILD_ADCSWAN=OFF -DBUILD_PADCSWAN=OFF  -DBUILD_PUNSWAN=OFF -DADDITIONAL_FLAGS_SWAN="-ffixed-line-length-73 -ffree-line-length-73" -DENABLE_OUTPUT_NETCDF=ON -DNETCDF_LIBRARIES="/opt/homebrew/Cellar/netcdf/4.9.0/bin" -DFortran_LINELENGTH_FLAG="-ffixed-line-length-132 -ffree-line-length-132" -DCMAKE_Fortran_FLAGS="-ffree-line-length-132  -ffixed-line-length-132 -O2" #-fallow-argument-mismatch -O2"
 
 # need to allow argument mismatch in modern fortran https://github.com/Unidata/netcdf-fortran/issues/212
 
