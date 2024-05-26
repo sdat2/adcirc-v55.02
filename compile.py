62bef51f4729c55b07dafc7bb6baba38e96ea7ed
Simon Thomas <sdat2@cam.ac.uk>
2024-05-26 11:49:52 +0100

    small tidy up

--- compile.py
+++
@@ -1,5 +0,0 @@
-import wandb
-
-wandb.init(project="adcirc-compiling", entity="sdat2")
-
-wandb.finish()
--- compile.sh
+++ compile.sh
@@ -2,12 +2,18 @@
 
 module load PrgEnv-gnu/8.3.3
 module load cray-hdf5-parallel/1.12.2.1
-module load cray-parallel-netcdf/1.12.3.1
+module load cray-netcdf-hdf5parallel/4.9.0.1
+
+#module load PrgEnv-gnu/8.3.3
+# export NETCDF_DIR=/opt/cray/pe/netcdf/4.9.0.1/gnu/9.1/
+# ncconfig="/opt/cray/pe/netcdf/4.9.0.1/gnu/9.1/bin/nc-config"
+# /opt/cray/pe/netcdf/4.9.0.1/gnu/9.1/bin/nc-config --version
 
 sw="-ffixed-line-length-72 -ffree-line-length-72"
 ll="-ffixed-line-length-132 -ffree-line-length-132"
-ncl="$(pnetcdf-config --libdir)"
-nci="$(pnetcdf-config --includedir)"
+nchome="$(nc-config --prefix)"
+ncl="$(nc-config --libdir)" # "$(${ncconfig} --libdir)"
+nci="$(nc-config --includedir)" # "$(${ncconfig} --includedir)"
 echo "NETCDF_LIBRARY_DIR: ${ncl}"
 echo "NETCDF_INCLUDE_DIR: ${nci}"
 
@@ -16,9 +22,9 @@ ftn --version
 which cc
 cc --version
 
-export NETCDF_DIR=/opt/cray/pe/parallel-netcdf/1.12.3.1/gnu/9.1/
 # export NETCDF_INCLUDE_DIR=/opt/cray/pe/parallel-netcdf/1.12.3.1/gnu/9.1/include
 # export NETCDF_LIBRARY=/opt/cray/pe/parallel-netcdf/1.12.3.1/gnu/9.1/lib
+
 export Fortran_LINELENGTH_FLAG="${ll}"
 
 cd adcirc
@@ -28,9 +34,11 @@ cmake .. --debug-output -DCMAKE_C_COMPILER=cc \
         -DCMAKE_CXX_COMPILER=cc \
         -DCMAKE_Fortran_COMPILER=ftn \
         -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=ON \
-        -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=OFF \
-        -DENABLE_OUTPUT_NETCDF=ON -DBUILD_PADCSWAN=OFF \
-        -DCMAKE_Fortran_FLAGS="-DREAL8 -DCSCA -DLINUX -w -fallow-argument-mismatch -O2" \
+        -DBUILD_ADCPREP=ON -DBUILD_ADCSWAN=ON \
+        -DENABLE_OUTPUT_NETCDF=ON -DBUILD_PADCSWAN=ON \
+        -DBUILD_ASWIP=ON -DBUILD_UTILITIES=ON \
+        -DBUILD_SWAN=ON -DBUILD_PUNSWAN=ON \
+        -DCMAKE_Fortran_FLAGS="-DREAL8 -DCSCA -DLINUX -g -fbacktrace -w -fallow-argument-mismatch -O2" \
         -DFortran_LINELENGTH_FLAG="${ll}" \
         -DADDITIONAL_FLAGS_SWAN="${swan}" \
         -DADDITIONAL_FLAGS_ADCPREP="${ll}"  \
@@ -39,5 +47,11 @@ cmake .. --debug-output -DCMAKE_C_COMPILER=cc \
         -DADDITIONAL_FLAGS_UTILITIES="${ll}" \
         -DNETCDFHOME="${NETCDF_DIR}" \
         -DNETCDF_LIBRARY="${ncl}" -DNETCDF_INCLUDE_DIR="${nci}" \
+        -DNETCDFHOME=$(nc-config --prefix) \
+        -DNETCDF_F90_LIBRARY="${ncl}/libnetcdff.so"
+
+        # -DNETCDF_F90_LIBRARY="/opt/cray/pe/netcdf/4.9.0.1/gnu/9.1/lib/libnetcdff.so"
+
+# -DNETCDFHOME=$(nc-config --prefix)
 
 make
